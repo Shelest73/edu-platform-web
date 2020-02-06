e4751fb1fa2f0b45cefa41f44d6c82e02cdcf509
Shelest73 <50738274+Shelest73@users.noreply.github.com>
2020-02-06 20:47:29 +0300

    Added project

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Education Platform</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="utf-8">
+</head>
+<body>
+
+	<header>
+	<div id="header">
+		<div id="logo_section">
+			<a href="" id="logo_link">
+				<img id="logo_img" src="images/Blender_logo.png" id="logo_image" alt="logo">
+				<p id="logo_text">Blender 2.8</p>
+			</a>
+		</div>
+		<a href="" id="log_in_section">Log in</a>
+	</div>
+	</header>
+
+	<div id="content">
+		<form id="search_form">
+			<input type="search" id="SearchField" name="SearchField" placeholder="Enter your request" size="10">
+			<!--<input type="submit" value="Search">-->
+		</form>
+		<div id="help_section">
+			<p id="help_title">Maybe you know...</p>
+			<div id="QuestionsBlock">
+				<a href="" class="help_item">How to do...?</a>
+				<a href="" class="help_item">How to do...?</a>
+				<a href="" class="help_item">How to do...?</a>
+				<a href="" class="help_item">How to do...?</a>
+				<a href="" class="help_item">How to do...?</a>
+			</div>	
+		</div>
+	</div>
+
+	<footer>
+		<div id="footer">
+			<div id="footer_section">
+				<p id="copyright">&copy;Obsessed Shaman, <?=date('Y')?></p>
+				<a href="">About Platform</a>
+				<a href="">Terms Of Service</a>
+				<a href="">Privacy Policy</a>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
